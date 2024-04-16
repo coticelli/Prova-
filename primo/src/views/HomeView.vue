d5c7af1f3f2a10442a2fa4ad4d66a32c08589220
coticelli <162303357+coticelli@users.noreply.github.com>
2024-04-16 10:01:03 +0000

    init

---
+++ primo/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+  </div>
+  <v-btn>
+    Button
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return { prodotti: [], 
+    }
+
+  },
+  methods: {
+  async getprodotti(){
+  await fetch
+
+  console.log(this.prodotti)
+  }
+},
+created(){
+
+},
+mounted(){
+
+}
+
+
+
+</script>
